f8503eff7a1ee5b46cc303b4b4b07f471be23118
maplbop135 <maplbop135@naver.com>
2020-06-20 11:45:52 +0900

    new file

--- #02.hello_world.c
+++ #02.hello_world.c
@@ -1,6 +1,9 @@
 /*
-"#02.hello_world.c"
-https://blog.naver.com/maplbop135?Redirect=Write&categoryNo=33
+Created by: Go keeper
+file name: "#02.hello_world.c"
+Blog link:
+    https://blog.naver.com/maplbop135?Redirect=Write&categoryNo=33
+    https://github.com/maplbop135/C_for_beginners/blob/master/%2302.hello_world.c
 
 input :
     none
