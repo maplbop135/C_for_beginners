34211b3bfebcc82ab8225e80f16372e0d835a4ac
maplbop135 <maplbop135@naver.com>
2020-06-20 10:34:06 +0900

    new file

--- hello_world.c
+++ hello_world.c
@@ -1,8 +1,18 @@
+/*
+
+C언어 익히기 #02. Hello World!
+"hello_world.c"
+
+input: none
+output: Hello World!
+
+*/
+
 #include <stdio.h>
 
 int main()
 {
-  printf("Hello World!");
+  printf("Hello World!\n");
 
   return 0;
 }
