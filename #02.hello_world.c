367d07f1896d67bec4d629c7eec31e2c0e577552
maplbop135 <maplbop135@naver.com>
2020-06-20 11:11:36 +0900

    new file

---
+++ #02.hello_world.c
@@ -0,0 +1,19 @@
+/*
+
+"#02.hello_world.c"
+https://blog.naver.com/maplbop135?Redirect=Write&categoryNo=33
+
+input: none
+output: Hello World!
+
+*/
+
+#include <stdio.h>
+
+// main function
+int main()
+{
+  printf("Hello World!\n");
+
+  return 0;
+}
