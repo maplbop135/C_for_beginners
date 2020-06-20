1e39036e1ef1c25ce411cf1b8346d3d1346fb80b
maplbop135 <maplbop135@naver.com>
2020-06-20 11:14:31 +0900

    new file

--- #02.hello_world.c
+++ #02.hello_world.c
@@ -1,11 +1,11 @@
 /*
-
 "#02.hello_world.c"
 https://blog.naver.com/maplbop135?Redirect=Write&categoryNo=33
 
-input: none
-output: Hello World!
-
+input :
+    none
+output:
+    Hello World!
 */
 
 #include <stdio.h>
