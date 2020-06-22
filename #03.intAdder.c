1c6508a3ed4b8fd3144c823ad1b040b6ba0aef7b
maplbop135 <maplbop135@naver.com>
2020-06-22 23:27:25 +0900

    new file

---
+++ #03.intAdder.c
@@ -0,0 +1,30 @@
+/*
+Created by: Go keeper
+file name: "#03.intAdder.c"
+Blog link:
+    https://blog.naver.com/maplbop135/222009227886
+
+
+input :
+    a, b
+output:
+    a + b
+*/
+
+#include <stdio.h>
+
+int intAdder(int a, int b){
+  return a + b;
+}
+
+int main()
+{
+  int num1, num2, result;
+
+  printf("Insert two numbers(ex> 10 20): ");
+  scanf("%d %d", &num1, &num2);
+  result = intAdder(num1, num2);
+  printf("%d + %d = %d\n", num1, num2, result);
+
+  return 0;
+}
